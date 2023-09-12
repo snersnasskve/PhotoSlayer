89c3979eb8772099ad646369a3db956967337d55
sners_nass <sners_nass@yahoo.co.uk>
2023-09-12 05:04:50 +0100

    write file

---
+++ writeFile.py
@@ -0,0 +1,11 @@
+import csv
+
+csv_filepath = "C:\\Users\\sners\\Desktop\\Team Turkey Run 2019\\TestOutput.csv"
+
+with open(csv_filepath, 'w', newline='') as csvfile:
+    fieldnames = ['File Name', 'File Path', 'MD5 Hash']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'File Name': 'photo.jpg', 'File Path': 'file_path', 'MD5 Hash': 'md5_hash'})
+
+      
